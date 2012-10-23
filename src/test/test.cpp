40732cc0e5fdffa2fb487ba1e68523130a869cd8
Chris_Laas <antlantisgate@gmail.com>
2012-10-23 14:50:25 +0200

    test.cpp cleaned

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -76,36 +76,7 @@ int main(int argc, char ** argv)
 	
 	
 	
-	/*#ifdef QRS
-		S3DViewer<S3DModelQRS> viewer;//Declaration of viewer
-		viewer.setOptions(true, false, true);
-	
-		S3DModelQRS *mods= new S3DModelQRS(model);
-		mods->mapJointToObs(fileParser->getJointNames(), jtsToPos);
-		#ifdef PART_MMSE
-		PartitionnedMMSE<S3DModelQRS, std::vector<std::vector<double> > > *lolilol = new PartitionnedMMSE<S3DModelQRS, std::vector<std::vector<double> > >(nbParticles, *mods);
-		#else
-		Partitionned<S3DModelQRS, std::vector<std::vector<double> > > *lolilol = new Partitionned<S3DModelQRS, std::vector<std::vector<double> > >(nbParticles, *mods);
-		#endif
-		std::vector<S3DModelQRS*> particles = lolilol->getParticleVector();
-		
-		IKSolverPFOrient<S3DModelQRS> iksol(particles, fileParser->getJointNames(), frame);//Declaration of solver
-	#else
-		S3DViewer<S3DModel> viewer;//Declaration of viewer
-		viewer.setOptions(true, false, true);
-		
-		S3DModel *mods= new S3DModel(model);
-		mods->mapJointToObs(fileParser->getJointNames(), jtsToPos);
-		#ifdef PART_MMSE
-		PartitionnedMMSE<S3DModel, std::vector<std::vector<double> > > *lolilol = new PartitionnedMMSE<S3DModel, std::vector<std::vector<double> > >(nbParticles, *mods);
-		#else
-		Partitionned<S3DModel, std::vector<std::vector<double> > > *lolilol = new Partitionned<S3DModel, std::vector<std::vector<double> > >(nbParticles, *mods);
-		#endif
-		std::vector<S3DModel*> particles = lolilol->getParticleVector();
-		
-		IKSolverPFOrient<S3DModel> iksol(particles, fileParser->getJointNames(), frame);//Declaration of solver
-	#endif
-	*/
+
 	
 	int nbParticles = env.getParticleNumber();
 	std::string filterType = env.getFilterType();
@@ -272,94 +243,7 @@ int main(int argc, char ** argv)
 		delete fileParser;
 	}
 	
-	/*
-	iksol.mapJointToObs(jtsToPos);
-	iksol.initFilter();
-	viewer.init();
 	
-	viewer.initModels(particles);
-	viewer.initObservations(fileParser->getJointNames(), frame);
-	iksol.computeLikelihood();
-	*/
-	
-	//******************************************
-	//**********END INITIALISATION**************
-	//******************************************
-	
-	/*std::vector<Eigen::Quaternionf*, Eigen::aligned_allocator<Eigen::Quaternionf*> > vec = mods[0]->getOrientationVec();
-	mods[0]->debug();
-	for (int i=0 ; i<vec.size() ; i++)
-	{
-		*vec[i]*=Eigen::Quaternionf(4,2,5,3);
-	}
-	mods[0]->debug();//*/
-	
-	//TESTER LE SAMPLING
-	
-	/*IKSolverPF iksol(mods);
-	
-	for(int i=0 ; i<1000 ; i++)
-	{
-		Eigen::Quaternionf mean;
-		mean.setIdentity();
-		viewer.displaySampling(iksol.sampleQuTEM(mean,1,1,0.1,1));
-	}*/
-	
-	
-	/*
-	//viewer.start(); //infinite loop
-	bool continuer = true;
-	std::string step = "InitFilter";
-	while (continuer)
-	{
-		//frame = fileParser->getNextFrame();//Observation update
-		if (step == "IK")
-		{
-			if (iksol.stepAlt() < 0.60)
-			{
-				iksol.save();
-				step = "InitFilter";
-			}
-			viewer.update(particles, frame);
-			continuer = viewer.isRendering();
-		}
-		else if (step == "InitFilter")
-		{
-			lolilol->init(frame);
-			step = "Filter";
-			nbFrames--;
-			viewer.update(particles, frame);
-			continuer = viewer.isRendering();
-		}
-		else if (step == "Filter")
-		{
-			frame = fileParser->getNextFrame();//Observation update
-			lolilol->step(frame);
-			nbFrames--;
-
-			viewer.update(particles, frame);
-			if (nbFrames==0)
-			{
-				step="End";
-				cout << "End of sequence" << endl;
-			}
-			continuer = viewer.isRendering();
-		}
-		else if (step == "End")
-		{
-			continuer = viewer.isRendering();
-		}
-		else
-		{
-			cout << "State unknown" << endl;
-			continuer = viewer.isRendering();
-		}
-	}
-	
-	delete lolilol;
-	delete mods;
-	delete fileParser;
-	*/
 	cout << "Program ended successfully !!!" << endl;
 	}
 	else
